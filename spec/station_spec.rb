cfb593e76807383134be0b4d7639ccc8cb25ceb5
bensheridanedwards <bensheridanedwards@gmail.com>
2019-11-20 16:44:11 +0000

    create station class and arguments

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,4 +2,16 @@ require "station"
 
 describe Station do
 
+  describe '#initialize' do 
+
+    it "should create a class instance of the station name" do 
+      station = Station.new("London Liverpool Street", 1)
+      expect(station.name).to eq "London Liverpool Street"
+    end
+
+    it "should create a class instance of the station zone" do
+      station = Station.new("London Liverpool Street", 1)
+      expect(station.zone).to eq 1
+    end
+  end
 end
