49b23c5a853f6b75c9e847fb12be2a7896cbef00
Alastair Gilles <agilles91@gmail.com>
2019-11-21 16:11:56 +0100

    Implemented Journey class

--- Gemfile
+++ Gemfile
@@ -3,5 +3,6 @@ source 'https://rubygems.org'
 ruby '2.6.3'
 
 group :development, :test do
+  gem 'rubocop'
   gem 'rspec'
 end
--- lib/journey.rb
+++ lib/journey.rb
@@ -1,3 +1,33 @@
 class Journey
+  PENALTY_FARE = 10
+  MINIMUM_FARE = 1
 
+  attr_reader :in_journey, :entry_station, :exit_station
+
+  def start station
+    @entry_station = station
+    @in_journey = true
+  end
+
+  def finish station
+    @exit_station = station
+    @in_journey = false
+  end
+
+  def fare
+    return MINIMUM_FARE if journey_complete?
+    return PENALTY_FARE if has_station?
+
+    0
+  end
+
+  private
+
+  def journey_complete?
+    !@in_journey && @entry_station && @exit_station
+  end
+
+  def has_station?
+    @entry_station || @exit_station
+  end
 end
